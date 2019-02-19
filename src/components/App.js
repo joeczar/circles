49c90540e3ae6819723b83ad5efcea98dd272cc9
Joe Czarnecki <joe.czar@gmail.com>
2019-02-19 10:49:22 +0100

    created circle react app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Circle } from '../icons/Circle';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className="ui container">
+                <Circle />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/icons/Circle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Circle = () => (
+
+    <svg 
+        width="200" 
+        height="200"
+    >
+        <g id="circle">
+            <circle cx="100" cy="100" r="80" stroke="black" stroke-width="1" fill="none" />
+        </g>
+    </svg> 
+)
